4c4684aebfd606b98613cbc89268cd40da84a398
ydrah <ydrah@f-lab.kr>
2023-10-29 17:53:34 +0900

    post 저장소를 app 인스턴스에서 접근할 수 있게 한다

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from starlette import status
 app = FastAPI()
 zone_info = ZoneInfo("Asia/Seoul")
 post_id = 0
-posts = {}
+app.posts = {}
 
 
 @app.get("/health", status_code=status.HTTP_200_OK)
@@ -47,7 +47,7 @@ def create_post(request: CreatePostRequest) -> CreatePostResponse:
         title=request.title,
         content=request.content,
     )
-    posts[post_id] = post
+    app.posts[post_id] = post
     post_id += 1
     return CreatePostResponse(
         id=post.id, title=post.title, content=post.content, created_at=post.created_at
@@ -65,7 +65,7 @@ class GetPostResponse(BaseModel):
     "/posts/{post_id}", response_model=GetPostResponse, status_code=status.HTTP_200_OK
 )
 def get_post(post_id: int) -> GetPostResponse:
-    post = posts.get(post_id)
+    post = app.posts.get(post_id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     return GetPostResponse(
@@ -85,7 +85,7 @@ def get_posts() -> List[GetPostResponse]:
             content=post.content,
             created_at=post.created_at,
         )
-        for post in posts.values()
+        for post in app.posts.values()
     ]
 
 
