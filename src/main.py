8ec60c9da08553a87588613e7cafa4c38c6f9164
ydrah <ydrah@f-lab.kr>
2023-10-29 17:42:19 +0900

    간단한 API를 가진 FastAPI 인스턴스를 추가한다

---
+++ src/main.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+from typing import List
+from zoneinfo import ZoneInfo
+
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from starlette import status
+
+app = FastAPI()
+zone_info = ZoneInfo("Asia/Seoul")
+post_id = 0
+posts = {}
+
+
+@app.get("/health", status_code=status.HTTP_200_OK)
+def health_check() -> str:
+    return "I'm alive!"
+
+
+class Post(BaseModel):
+    id: int
+    title: str
+    content: str
+    created_at: datetime = datetime.now(tz=zone_info)
+
+
+class CreatePostRequest(BaseModel):
+    title: str
+    content: str
+
+
+class CreatePostResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+    created_at: datetime
+
+
+@app.post(
+    "/posts", response_model=CreatePostResponse, status_code=status.HTTP_201_CREATED
+)
+def create_post(request: CreatePostRequest) -> CreatePostResponse:
+    global post_id
+    post = Post(
+        id=post_id,
+        title=request.title,
+        content=request.content,
+    )
+    posts[post_id] = post
+    post_id += 1
+    return CreatePostResponse(
+        id=post.id, title=post.title, content=post.content, created_at=post.created_at
+    )
+
+
+class GetPostResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+    created_at: datetime
+
+
+@app.get(
+    "/posts/{post_id}", response_model=GetPostResponse, status_code=status.HTTP_200_OK
+)
+def get_post(post_id: int) -> GetPostResponse:
+    post = posts.get(post_id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return GetPostResponse(
+        id=post.id,
+        title=post.title,
+        content=post.content,
+        created_at=post.created_at,
+    )
+
+
+@app.get("/posts", response_model=List[GetPostResponse], status_code=status.HTTP_200_OK)
+def get_posts() -> List[GetPostResponse]:
+    return [
+        GetPostResponse(
+            id=post.id,
+            title=post.title,
+            content=post.content,
+            created_at=post.created_at,
+        )
+        for post in posts.values()
+    ]
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
